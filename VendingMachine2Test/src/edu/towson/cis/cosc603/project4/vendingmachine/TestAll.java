ec811249b2e5950414698b4e74228727bde5c6ac
MarleneRZ <lynn2301@verizon.net>
2016-04-10 12:42:39 -0400

    Task 2 - After generating TestCases with codePro and creating TestReport2 -still not 100% coverage

---
+++ VendingMachine2Test/src/edu/towson/cis/cosc603/project4/vendingmachine/TestAll.java
@@ -0,0 +1,34 @@
+package edu.towson.cis.cosc603.project4.vendingmachine;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * The class <code>TestAll</code> builds a suite that can be used to run all
+ * of the tests within its package as well as within any subpackages of its
+ * package.
+ *
+ * @generatedBy CodePro at 4/10/16 12:25 PM
+ * @author MarleneRZ
+ * @version $Revision: 1.0 $
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	VendingMachineExceptionTest.class,
+	VendingMachineTest.class,
+	VendingMachineItemTest.class,
+})
+public class TestAll {
+
+	/**
+	 * Launch the test.
+	 *
+	 * @param args the command line arguments
+	 *
+	 * @generatedBy CodePro at 4/10/16 12:25 PM
+	 */
+	public static void main(String[] args) {
+		JUnitCore.runClasses(new Class[] { TestAll.class });
+	}
+}
