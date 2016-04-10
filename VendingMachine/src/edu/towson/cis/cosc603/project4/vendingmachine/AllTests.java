69cf9924a331b898e20218d93a83ae710ab24e8c
MarleneRZ <lynn2301@verizon.net>
2016-04-10 11:19:18 -0400

    Initial Commit

---
+++ VendingMachine/src/edu/towson/cis/cosc603/project4/vendingmachine/AllTests.java
@@ -0,0 +1,22 @@
+package edu.towson.cis.cosc603.project4.vendingmachine;
+
+import org.junit.runner.RunWith;
+
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+//import junit.framework.JUnit4TestAdapter;
+
+
+/**
+ *Create a test suite called AllTests.java which runs all the unit tests as a test suite
+ * @author MarleneRZ
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ VendingMachineItemTest.class, VendingMachineTest.class })
+public class AllTests {
+	
+    }
+
+
+
